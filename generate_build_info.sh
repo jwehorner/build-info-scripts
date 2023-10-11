4052c3e660f771163be0ba5254a05750dab2a8ef
James Horner <James.Horner@nrc-cnrc.gc.ca>
2023-10-11 12:28:28 -0400

    (Feat) Added bash version with header guard, and added header guard to windows version.

--- generate_build_info.sh
+++ generate_build_info.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-$(
-	git log --pretty=format:"#define GIT_LAST_COMMIT_DATE    \"%ad\"%n#define GIT_LAST_COMMIT_HASH    \"%H\"%n#define GIT_LAST_COMMIT_MESSAGE \"%s\" %n" -n 1
-)> build_info.h
 
-set status_text=
-set row=
+echo "#ifndef BUILD_INFO_H" > build_info.h
+echo "#define BUILD_INFO_H" >> build_info.h
+echo "" >> build_info.h
+
+git_log_format="#define GIT_LAST_COMMIT_DATE    \"%ad\"%n#define GIT_LAST_COMMIT_HASH    \"%H\"%n#define GIT_LAST_COMMIT_MESSAGE \"%s\" %n"
+git log --pretty=format:"${git_log_format}" -n 1 >> build_info.h
+
+status_text=
+row=
 IFS=$'\n'
-for tbl in $(git status -s)
+for row in $(git status -s)
 do
-	set status_text=%status_text% %row%
+	status_text="${status_text}${row}"
 done
 
-echo #define GIT_CURRENT_CHANGES		"$status_text" >> build_info.h
-echo #define CURRENT_BUILD_TIME 		"%DATE%%TIME%" >> build_info.h
-echo #define CURRENT_BUILD_USER		"%USERNAME%" >> build_info.h
-echo #define CURRENT_BUILD_COMPUTER	"%COMPUTERNAME%" >> build_info.h
+echo "#define GIT_CURRENT_CHANGES		\"${status_text}\"" >> build_info.h
+echo "#define CURRENT_BUILD_TIME 		\"$(date)\"" >> build_info.h
+echo "#define CURRENT_BUILD_USER		\"$(whoami)\"" >> build_info.h
+echo "#define CURRENT_BUILD_COMPUTER	\"$(hostname)\"" >> build_info.h
+
+echo "" >> build_info.h
+echo "#endif /* BUILD_INFO_H */" >> build_info.h
