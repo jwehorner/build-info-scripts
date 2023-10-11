9b040a67b3229494987ed8f2fbbddcfdbc3ec526
James Horner <james.horner@nrc-cnrc.gc.ca>
2023-10-11 12:34:39 -0400

    (Fix) Fixed git log format on Windows.

--- generate_build_info.bat
+++ generate_build_info.bat
@@ -1,24 +1,24 @@
 @echo off
 setlocal enabledelayedexpansion
 
-(echo "#ifndef BUILD_INFO_H") > build_info.h
-(echo "#define BUILD_INFO_H") >> build_info.h
-(echo "") >> build_info.h
+(echo #ifndef BUILD_INFO_H) > build_info.h
+(echo #define BUILD_INFO_H) >> build_info.h
+(echo.) >> build_info.h
 
+set git_log_format="#define GIT_LAST_COMMIT_DATE    \"%%ad\"%%n#define GIT_LAST_COMMIT_HASH    \"%%H\"%%n#define GIT_LAST_COMMIT_MESSAGE \"%%s\" %%n"
 (
-	set git_log_format="#define GIT_LAST_COMMIT_DATE    \"%%ad\"%%n#define GIT_LAST_COMMIT_HASH    \"%%H\"%%n#define GIT_LAST_COMMIT_MESSAGE \"%%s\" %%n"
-	git log --pretty=format:"%git_log_format" -n 1
+	git log --pretty=format:%git_log_format% -n 1
 ) >> build_info.h
 
 set status_text=
 set row=
 for /F "tokens=*" %%x in ('git status -s') do set "row=!row! %%x"
-set status_text=%status_text% %row%
+set status_text=%status_text%%row%
 
 (echo #define GIT_CURRENT_CHANGES		"%status_text%") 	>> build_info.h
 (echo #define CURRENT_BUILD_TIME 		"%DATE%%TIME%") 	>> build_info.h
 (echo #define CURRENT_BUILD_USER		"%USERNAME%") 		>> build_info.h
 (echo #define CURRENT_BUILD_COMPUTER	"%COMPUTERNAME%") 	>> build_info.h
 
-(echo "") >> build_info.h
-(echo "#endif /* BUILD_INFO_H */") >> build_info.h
+(echo.) >> build_info.h
+(echo #endif /* BUILD_INFO_H */) >> build_info.h
